08e71e49ff5d5339abe544cad02e3a1384647312
Xersan <140951375+Xersan@users.noreply.github.com>
2023-10-17 23:37:01 +0300

    create user entity model

---
+++ src/main/java/com/eurodyn/simplewebapp/models/User.java
@@ -0,0 +1,24 @@
+package com.eurodyn.simplewebapp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String surname;
+    private char gender;
+    private String date;
+    private String workAddress;
+    private String homeAddress;
+
+}
