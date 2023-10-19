1fe8ffc41bacfa74c0f7466750fea252c2d38622
Xersan <140951375+Xersan@users.noreply.github.com>
2023-10-19 20:55:39 +0300

    implement get all users (get) and register new user (post) in javascript

--- src/main/java/com/eurodyn/simplewebapp/models/Address.java
+++ src/main/java/com/eurodyn/simplewebapp/models/Address.java
@@ -14,9 +14,11 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "work_address")
     @Size(max = 250, message = "Address must be 250 characters max.")
     private String workAddress;
 
+    @Column(name = "home_address")
     @Size(max = 250, message = "Address must be 250 characters max.")
     private String homeAddress;
 }
--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,3 +1,81 @@
 $(document).ready(function () {
     console.log("ready");
+
+    $("#register-form").hide();
+    $("#users-table").hide();
+
+    $(document).on("click", "#display-form", function () {
+        $("#register-form").show(350);
+    });
+
+    $(document).on("click", "#display-users", function () {
+        let jsonString = {};
+
+        $.ajax({
+            type: 'GET',
+            url: "/api/v1/users/all",
+            contentType: 'application/json',
+            data: jsonString,
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+
+                let table = new Tabulator("#users-table", {
+                    data:data,
+                    movableColumns:false,
+                    layout:"fitDataFill",
+                    columns:[
+                        {title:"Name", field:"name"},
+                        {title:"Surname", field:"surname"},
+                    ],
+                });
+
+                $("#users-table").show(500);
+
+                table.on("rowClick", function(e, row){
+                    alert("Row " + row.getData().id + " Clicked!!!!");
+                });
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+    });
+
+    $("#submit-form").click(function (e) {
+        e.preventDefault();
+        let obj = {};
+        let obj2 = {};
+        obj.name = $("#input-name").val();
+        obj.surname = $("#input-surname").val();
+        obj.gender = $("#input-gender").val();
+        obj.date = $("#input-date").val();
+        let workAddress = $("#input-work-address").val();
+        let homeAddress = $("#input-home-address").val();
+        if (!(workAddress === "" && homeAddress === ""))
+        {
+            obj2.workAddress = workAddress;
+            obj2.homeAddress = homeAddress;
+            obj.address = obj2;
+        }
+
+        let jsonString = JSON.stringify(obj);
+        console.log("jsonstring", jsonString);
+
+        $.ajax({
+            type: 'POST',
+            url: "/api/v1/users/add",
+            contentType: 'application/json',
+            data: jsonString,
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+
+    });
+
 })
